589899e0d2b3dce01ae37390f85e6b3f7bd8e659
Andrew Wigginton <andrewwigginton@Andrews-MacBook-Pro.local>
2019-03-08 23:35:57 -0700

    added lap feature/css/removed commented code

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
-import stopwatch from './stopwatch.svg';
-import './App.css';
+import React, { Component } from "react";
+import stopwatch from "./stopwatch.svg";
+import "./App.css";
 
 class App extends Component {
   constructor() {
@@ -12,74 +12,116 @@ class App extends Component {
       startEnabled: true,
       stopEnabled: true,
       clearEnabled: true,
-
-      lap: []
+      lapEnabled: false,
+      laps: []
     };
   }
   getSeconds = () => {
-    return ('0' + this.state.secondsElapsed % 60).slice(-2)
-  }
+    return ("0" + (this.state.secondsElapsed % 60)).slice(-2);
+  };
 
   getMinutes = () => {
-    return Math.floor(this.state.secondsElapsed / 60)
-  }
+    return Math.floor(this.state.secondsElapsed / 60);
+  };
 
   handleStartTimer = () => {
     this.setState({
-      startEnabled: false, stopEnabled: true,
+      startEnabled: false,
+      stopEnabled: true,
       clearEnabled: true,
-    })
+      lapEnabled: true
+    });
     let _this = this;
     this.incrementer = setInterval(() => {
       _this.setState({
-        secondsElapsed: (this.state.secondsElapsed + 1)
-      })
-
-    }, 1000)
-
-  }
+        secondsElapsed: this.state.secondsElapsed + 1
+      });
+    }, 1000);
+  };
 
   handleStopTimer = () => {
     this.setState({
-      startEnabled: true, stopEnabled: false,
+      startEnabled: true,
+      stopEnabled: false,
       clearEnabled: true,
-    })
-    clearInterval(this.incrementer)
-
-  }
+      lapEnabled: false
+    });
+    clearInterval(this.incrementer);
+  };
 
   handleClearTimer = () => {
     this.setState({
-      startEnabled: true, stopEnabled: true,
+      startEnabled: true,
+      stopEnabled: true,
       clearEnabled: false,
-    })
+      lapEnabled: false
+    });
     clearInterval(this.incrementer);
-    this.setState({ secondsElapsed: 0 })
+    this.setState({ secondsElapsed: 0, laps: [] });
+  };
 
-  }
-
+  handleLap = val => {
+    let accLaps = this.state.laps;
+    accLaps.push(val);
+    this.setState({ laps: accLaps });
+    console.log(this.state.laps);
+  };
 
   render() {
-
+    let displayLaps = this.state.laps.map((lap, index) => {
+      return (
+        <div key={lap} className="lap_container">
+          <p className="lap_number">{`Lap ${index + 1}`}</p>
+          <p className="lap_number">|</p>
+          <p className="lap_time">{lap}</p>
+        </div>
+      );
+    });
     return (
       <div className="App">
         <div className="stopwatch_container">
           <div className="icon_title_container">
-            {/* <h4 className="title">STOP</h4> */}
             <img src={stopwatch} alt="stopwatch logo" className="stopwatch" />
-            <h4 className="title">STOPWATCH</h4></div>
-          {/* <img src={stopwatch} alt="stopwatch logo" className="stopwatch" />
-        <h4 className="title">STOPWATCH</h4> */}
-          <p className="time">{this.getMinutes()}:{this.getSeconds()}</p>
-          {/* <button onClick={() => this.startTimer()}>{this.state.running ? 'Stop' : 'Start'}</button>
-        <button onClick={() => this.startTimer()}>{this.state.running ? 'Stop' : 'Start'}</button> */}
-          <button className="start" disabled={!this.state.startEnabled} onClick={() => this.handleStartTimer()}>START</button>
-          <button className="stop" disabled={!this.state.stopEnabled} onClick={() => this.handleStopTimer()}>STOP</button>
+            <h4 className="title">STOPWATCH</h4>
+          </div>
+          <p className="time">
+            {this.getMinutes()}:{this.getSeconds()}
+          </p>
+          <button
+            className="start"
+            disabled={!this.state.startEnabled}
+            onClick={() => this.handleStartTimer()}
+          >
+            START
+          </button>
+          <button
+            className="stop"
+            disabled={!this.state.stopEnabled}
+            onClick={() => this.handleStopTimer()}
+          >
+            STOP
+          </button>
           <button
             className="clear"
             disabled={!this.state.clearEnabled}
-            onClick={() => this.handleClearTimer()}> CLEAR</button>
-        </div></div >
+            onClick={() => this.handleClearTimer()}
+          >
+            {" "}
+            CLEAR
+          </button>
+          <button
+            className="lap_button"
+            disabled={this.state.startEnabled}
+            onClick={() =>
+              this.handleLap(`${this.getMinutes()}:${this.getSeconds()}`)
+            }
+          >
+            {" "}
+            LAP{" "}
+          </button>
+          <div className="laps_container">{displayLaps}</div>
+        </div>
+      </div>
     );
   }
 }
