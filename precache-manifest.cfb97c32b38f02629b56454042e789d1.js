95cea55b4538f654b3d345307fc1f2cb84673b7c
Andrew Wigginton <andrewwigginton@Andrews-MacBook-Pro.local>
2019-03-12 12:22:48 -0600

    Updates

---
+++ precache-manifest.cfb97c32b38f02629b56454042e789d1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e9c151313148d7a90a1831852cd4776a",
+    "url": "/stopwatch/static/media/stopwatch.e9c15131.svg"
+  },
+  {
+    "revision": "7fa7798e0f83aafb75f7",
+    "url": "/stopwatch/static/js/runtime~main.84f376ef.js"
+  },
+  {
+    "revision": "b4e10bd424d6d472c341",
+    "url": "/stopwatch/static/js/main.ea679897.chunk.js"
+  },
+  {
+    "revision": "82386e3cc7ff629b0918",
+    "url": "/stopwatch/static/js/2.e3fea541.chunk.js"
+  },
+  {
+    "revision": "b4e10bd424d6d472c341",
+    "url": "/stopwatch/static/css/main.fb80694c.chunk.css"
+  },
+  {
+    "revision": "fad87cc5da3045a59418dad6f71ace7a",
+    "url": "/stopwatch/index.html"
+  }
+];
